656d72371ea9240c96e5c1e4ec256302565bef2c
Jennifer Minnich <jminniecc@gmail.com>
2017-01-15 12:07:38 -0700

    commit content: purpose, audience, goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -12,14 +12,8 @@
 			<!-- img src="" insert QUAY graphic here-->
 			<h2>Purpose</h2>
 				<p>The objective of my Personal Website (PWP) is to visually and efficiently market my web development skills and introduce my company Quay.cncpts. I want to give the viewer a better understanding of who I am in hopes of obtaining future contracts in this field! The challenge I am attempting is strategically marrying my previous career in product design with my current position in web development.</p>
+			<p>I want to captivate any potential clientele viewing my site with awareness and interest! This should be done with a site that appears professional and versatile but also reflecting my personal style. In the spirit of networking and collaboration, I would also like for my site to attract and invite other developers to contact me!</p>
+			<p>Ultimately, I want my site to advertise my web development business platform in addition to my previous design career. I want to maintain a high- end aesthetic suitable for design minded clients who might (or might not) be more visually inclined, have a design/ innovation to advertise, an image/ video gallery focus, or a product to sell. In short, encapsulating my goals as "Design & Development" services. </p>
 		</main>
 	</body>
 </html>
-Audience: Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of? Could it be for your professional peers and other developers, potential employers, customers, or clients, etc.?
-Goal: T
-	his is should be a quantifiable goal if possible. For example, a goal for your PWP could be:
-To acquire full or part-time employment or an internship.
-To increase sales and/or conversion rates for an existing business.
-To attract freelance clients.
-To serve as a gallery to showcase and promote your work.
-To increase the reach of a new or established social media presence (as measured via analytics tools).
