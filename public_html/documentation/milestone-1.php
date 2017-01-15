60d1a4de4c229519a5c2e010b27657a00aa21ce7
Jennifer Minnich <jminniecc@gmail.com>
2017-01-15 11:04:04 -0700

    commit revised README

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,7 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mac
- * Date: 1/13/17
- * Time: 12:04 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>QUAY.cncpts</title>
+	</head>
+	<body>
+		<header>
+			<h1>QUAY.cncpts</h1>
+		</header>
+		<main>
+			<!-- img src="" insert QUAY graphic here-->
+			<h2>Purpose</h2>
+				<p>The objective of my Personal Website (PWP) is to visually and efficiently market my web development skills and introduce my company Quay.cncpts. I want to give the viewer a better understanding of who I am in hopes of obtaining future contracts in this field! The challenge I am attempting is strategically marrying my previous career in product design with my current position in web development.</p>
+		</main>
+	</body>
+</html>
+Audience: Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of? Could it be for your professional peers and other developers, potential employers, customers, or clients, etc.?
+Goal: T
+	his is should be a quantifiable goal if possible. For example, a goal for your PWP could be:
+To acquire full or part-time employment or an internship.
+To increase sales and/or conversion rates for an existing business.
+To attract freelance clients.
+To serve as a gallery to showcase and promote your work.
+To increase the reach of a new or established social media presence (as measured via analytics tools).
