dab89cefdba9349d10447e6170d944aba704159b
Jennifer Minnich <jminniecc@gmail.com>
2017-02-05 13:15:46 -0700

    commit milestone 2a wire frame mobile

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -11,4 +11,7 @@
 		<main>
 			<img src="../images/quay.jpg" alt="quay concepts" align="center" width="45%" height="50%">
 			<h2>Content Strategy</h2>
-			<p>Minimalism. I am a firm believer that less is more! I am opposed to navigating a busy interface and therefore will implement a clean styling strategy into my personal website. </p>
+			<p>Minimalism.</p>
+			<p>I am a firm believer that less is more. Nothing is worse then navigating a busy interface and therefore I will implement a clean styling strategy into my personal website. For example: white ground, dark text for content, accent pop colors for menus, predominant images and straightforward button navigation.  </p>
+			<h2>Wire Frames</h2>
+			<img src="../images/mobile1.jpg.jpg" alt="mobile wire frame" align="center" width="45%" height="50%">
