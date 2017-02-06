d0358ab45b584ff35f7218e869eedb8f803e7ab5
Jennifer Minnich <jminniecc@gmail.com>
2017-02-05 21:48:03 -0700

    commit milestone 2a wire frame desktop

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -11,7 +11,12 @@
 		<main>
 			<img src="../images/quay.jpg" alt="quay concepts" align="center" width="45%" height="50%">
 			<h2>Content Strategy</h2>
-			<p>Minimalism.</p>
-			<p>I am a firm believer that less is more. Nothing is worse then navigating a busy interface and therefore I will implement a clean styling strategy into my personal website. For example: white ground, dark text for content, accent pop colors for menus, predominant images and straightforward button navigation.  </p>
+			<p>Welcome</p>
+			<p>Minimalism.I am a firm believer that less is more. Nothing is worse then navigating a busy interface. I will implement a clean and modern styling strategy into my personal website.</p>
+			<p>Possible Style Elements: white ground, dark text for content, accent pop colors for menus, predominant images and straightforward button navigation for easy use.</p>
 			<h2>Wire Frames</h2>
-			<img src="../images/mobile1.jpg.jpg" alt="mobile wire frame" align="center" width="45%" height="50%">
+			<img src="../images/mobile1.jpg" alt="mobile wire frame" align="center" width="45%" height="50%">
+			<img src="../images/desktop.jpg" alt="mobile wire frame" align="center" width="45%" height="50%">
+		</main>
+	</body>
+</html>
