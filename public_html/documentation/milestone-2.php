62f134899173b7ca7892ea2c2127bc76db0d481e
Jennifer Minnich <jminniecc@gmail.com>
2017-02-06 07:10:45 -0700

    commit milestone 2a wire frame desktop1 & 2 revised & content strategy revised

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -11,12 +11,16 @@
 		<main>
 			<img src="../images/quay.jpg" alt="quay concepts" align="center" width="45%" height="50%">
 			<h2>Content Strategy</h2>
-			<p>Welcome</p>
-			<p>Minimalism.I am a firm believer that less is more. Nothing is worse then navigating a busy interface. I will implement a clean and modern styling strategy into my personal website.</p>
-			<p>Possible Style Elements: white ground, dark text for content, accent pop colors for menus, predominant images and straightforward button navigation for easy use.</p>
+			<p>Minimalism. One page site with a clean and modern styling strategy. I am a firm believer that less is more. Nothing is worse then navigating a busy interface!</p>
+			<p>Style Elements: white ground, dark text for content, accent pop colors for menus, predominant images and straightforward button navigation for easy use.</p>
+			<p>Welcome: Big photographs and bold titles</p>
+			<p>CV: Experience, resume</p>
+			<p>Portfolio: Thumbnails of design and development work. Image can be selectd and viewed larger.</p>
+			<p>Contact: contact form and favicon icons with social media links</p>
 			<h2>Wire Frames</h2>
 			<img src="../images/mobile1.jpg" alt="mobile wire frame" align="center" width="45%" height="50%">
-			<img src="../images/desktop.jpg" alt="mobile wire frame" align="center" width="45%" height="50%">
+			<img src="../images/desktop1.jpg" alt="mobile wire frame" align="center" width="45%" height="50%">
+			<img src="../images/desktop2.jpg" alt="mobile wire frame" align="center" width="45%" height="50%">
 		</main>
 	</body>
 </html>
